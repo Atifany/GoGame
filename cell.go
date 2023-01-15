9598c6a9f7dec964af5570867b2640c0a7eaffe8
Atifany <bordackov.misha2014@yandex.ru>
2023-01-15 17:01:37 +0300

    added two simple tiles a general drawing

--- cell.go
+++ cell.go
@@ -1,13 +1,38 @@
 package main
 
 import (
-	"github.com/hajimehoshi/ebiten/v2"
+	"math"
 )
 
+type Point struct {
+	x float64
+	y float64
+}
+
 type Cell struct {
-	sprite *ebiten.Image
-	x      float64
-	y      float64
-	scaleX float64
-	scaleY float64
+	sprite    *MyImage
+	position  Point
+	direction float64
+	cellType  int
+}
+
+// Returns a pointer to a tile which occupies targetPos. Returns nil of none were found
+func checkCollisions(cells []*Cell, targetPos Point) *Cell {
+	for _, cell := range cells {
+		if (*cell).position.x == targetPos.x && (*cell).position.y == targetPos.y {
+			return cell
+		}
+	}
+	return nil
+}
+
+// Moves its tile by one width in the direction pointed by Cell.direction
+func (c *Cell) moveForwardOne(cells []*Cell) {
+	target := Point{math.Round(c.position.x + 1*math.Cos(c.direction)),
+		math.Round(c.position.y + 1*math.Sin(c.direction))}
+	if checkCollisions(cells, target) != nil {
+		return
+	}
+
+	c.position = target
 }
--- main.go
+++ main.go
@@ -2,60 +2,137 @@ package main
 
 import (
 	"log"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
+type MyImage struct {
+	image *ebiten.Image
+	size  Point
+}
+
 type Game struct{}
 
 const screenWidth int = 1024
 const screenHeight int = 768
-const SCALE int = 1
+const SCALE float64 = 2.0
+const updatesPerCall int = 30
 
-var sampleImage *ebiten.Image
+const moveStraightCell int = 0
+const wallCell int = 1
 
-var cell Cell
+var updatesElapsed int = 0
 
-var entities []interface{}
+var cells []*Cell
 
 // Called after RunGame is called
 func init() {
-	sampleImage = LoadImage("./Sample-PNG-Image.png")
+	redTileImage := LoadImage("./textures/RedTile.png")
+	blackTileImage := LoadImage("./textures/BlackTile.png")
 
-	cell = Cell{sampleImage, 0, 0, 0.15, 0.15}
-	entities = append(entities, cell)
+	cells = append(cells, &Cell{redTileImage,
+		Point{1, 1}, 0.0, moveStraightCell})
+
+	cells = append(cells, &Cell{redTileImage,
+		Point{2, 2}, math.Pi / 2, moveStraightCell})
+
+	cells = append(cells, &Cell{blackTileImage,
+		Point{2, 3}, 0.0, wallCell})
+
+	cells = append(cells, &Cell{blackTileImage,
+		Point{4, 1}, 0.0, wallCell})
+
+	cells = append(cells, &Cell{redTileImage,
+		Point{6, 6}, 0.0, moveStraightCell})
+
+	cells = append(cells, &Cell{redTileImage,
+		Point{8, 6}, math.Pi, moveStraightCell})
+
+	cells = append(cells, &Cell{redTileImage,
+		Point{6, 7}, 0.0, moveStraightCell})
+
+	cells = append(cells, &Cell{redTileImage,
+		Point{9, 7}, math.Pi, moveStraightCell})
+
+	cells = append(cells, &Cell{redTileImage,
+		Point{6, 8}, 0.0, moveStraightCell})
+
+	cells = append(cells, &Cell{redTileImage,
+		Point{7, 8}, 0.0, moveStraightCell})
 }
 
-func LoadImage(path string) *ebiten.Image {
-	img, _, err := ebitenutil.NewImageFromFile(path)
+// Loads png file into ebiten.Image struct
+func LoadImage(path string) *MyImage {
+	img, i, err := ebitenutil.NewImageFromFile(path)
 	if err != nil {
 		log.Fatal(err.Error())
 		return nil
 	}
-	return img
+
+	return &MyImage{img, Point{float64(i.Bounds().Max.X), float64(i.Bounds().Max.Y)}}
+}
+
+func ProcedeCells() {
+	for _, cell := range cells {
+		switch (*cell).cellType {
+		case moveStraightCell:
+			(*cell).moveForwardOne(cells)
+		}
+	}
 }
 
 // Called every frame
 func (g *Game) Update() error {
-	cell.x += 0.5
-	cell.y += 0.4
+	if updatesElapsed < updatesPerCall {
+		updatesElapsed++
+		return nil
+	}
+	updatesElapsed = 0
+
+	ProcedeCells()
+
 	return nil
+}
+
+/*
+	Summary
+
+because op.GeoM.Rotate rotates an image the wrong way this function
+is needed to adjust rotation result.
+It also moves image to cell's coordinates
+*/
+func adjustAfterRotation(c *Cell, op *ebiten.DrawImageOptions) {
+	cos := math.Cos((*c).direction)
+	sin := math.Sin((*c).direction)
+
+	rot := Point{cos - sin, sin + cos}
+	if rot.x >= 0 {
+		rot.x = 0.0
+	}
+	if rot.y >= 0 {
+		rot.y = 0.0
+	}
+
+	op.GeoM.Translate(math.Round(((*c).position.x-rot.x)*(*c).sprite.size.x),
+		math.Round(((*c).position.y-rot.y)*(*c).sprite.size.y))
 }
 
 // Called every frame to draw
 func (g *Game) Draw(screen *ebiten.Image) {
-	for _, entity := range entities {
+	for _, cell := range cells {
 		op := &ebiten.DrawImageOptions{}
-		op.GeoM.Translate(float64(entity.(Cell).x), float64(entity.(Cell).y))
-		op.GeoM.Scale(entity.(Cell).scaleX, entity.(Cell).scaleY)
-		screen.DrawImage(sampleImage, op)
+		op.GeoM.Rotate((*cell).direction)
+		adjustAfterRotation(cell, op)
+
+		screen.DrawImage((*cell).sprite.image, op)
 	}
 }
 
 // whatever
 func (g *Game) Layout(outsideWidth int, outsideHeight int) (int, int) {
-	return screenWidth / SCALE, screenHeight / SCALE
+	return int(float64(screenWidth) / SCALE), int(float64(screenHeight) / SCALE)
 }
 
 func main() {
